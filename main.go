be48eaeda3729c01f1d1011312fec22b5b4e6219
GRACENOBLE <gracenoble72@gmail.com>
2024-11-05 20:51:53 +0300

    chore (structure): project structure

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main(){
-	
+	reader := bufio.NewReader(os.Stdin)
+	for{
+
+		fmt.Print("expense-tracker:")
+		
+		input, _ := reader.ReadString('\n')
+ 
+		parts := strings.Fields(input)
+		
+		switch parts[0]{
+
+		case "add":
+			
+		case "list":
+		case "summary":
+		case "delete":
+			
+		}
+	}
 }
