dec7ffef559478b0642008f28ae629a53c368de0
GRACE NOBLE <gracenoble72@gmail.com>
2024-11-09 21:28:33 +0300

    feat(progress): boilerplate

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,10 +1,31 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+
 	"github.com/GRACENOBLE/expense-tracker/pkg/types"
 )
 
-func DeleteExpense(expense types.Expense){
-	fmt.Println("Deleted")
+func DeleteExpense(index int64){
+
+	jsonData, err := os.ReadFile("output/expenses.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var expenses []types.Expense
+	
+	err = json.Unmarshal(jsonData, &expenses)
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	for _, expense := range expenses{
+		
+		fmt.Printf("%v", expense)
+	}
+
 }
