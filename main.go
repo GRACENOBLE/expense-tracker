981183a5096a5fec47bf0c2f92632a5a044f6bcf
GRACENOBLE <gracenoble72@gmail.com>
2024-11-10 19:06:00 +0300

    feat (mod): added cobra dependency

--- main.go
+++ main.go
@@ -1,55 +1,57 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"github.com/GRACENOBLE/expense-tracker/cmd"
-	"log"
-	"os"
-	"strconv"
-	"strings"
-)
+// import (
+// 	"bufio"
+// 	"fmt"
+// 	"github.com/GRACENOBLE/expense-tracker/cmd"
+// 	"log"
+// 	"os"
+// 	"strconv"
+// 	"strings"
+// )
+
+import "github.com/spf13/cobra"
 
 func main() {
 
-	reader := bufio.NewReader(os.Stdin)
-	for {
+	// reader := bufio.NewReader(os.Stdin)
+	// for {
 
-		fmt.Print("expense-tracker:")
+	// 	fmt.Print("expense-tracker:")
 
-		input, _ := reader.ReadString('\n')
+	// 	input, _ := reader.ReadString('\n')
 
-		parts := strings.Fields(input)
+	// 	parts := strings.Fields(input)
 
-		switch parts[0] {
+	// 	switch parts[0] {
 
-		case "add":
-			if len(parts) == 3 {
+	// 	case "add":
+	// 		if len(parts) == 3 {
 
-				ammount, err := strconv.ParseInt(parts[2], 10, 64)
-				if err != nil {
-					log.Fatal(err)
-				}
-				cmd.AddExpense(parts[1], ammount)
+	// 			ammount, err := strconv.ParseInt(parts[2], 10, 64)
+	// 			if err != nil {
+	// 				log.Fatal(err)
+	// 			}
+	// 			cmd.AddExpense(parts[1], ammount)
 
-				return
+	// 			return
 
-			} 
+	// 		} 
 
-			fmt.Println("Invalid command-line Arguments")
+	// 		fmt.Println("Invalid command-line Arguments")
 			
-		case "list":
+	// 	case "list":
 
-			cmd.ListExpenses()
+	// 		cmd.ListExpenses()
 
-		case "summary":
+	// 	case "summary":
 
-			cmd.SummarizeExpenses()
+	// 		cmd.SummarizeExpenses()
 
-		case "delete":
-			// cmd.DeleteExpense()
-		default:
-			fmt.Println("Command not recognized, try \"help\"")
-		}
-	}
+	// 	case "delete":
+	// 		// cmd.DeleteExpense()
+	// 	default:
+	// 		fmt.Println("Command not recognized, try \"help\"")
+	// 	}
+	// }
 }
