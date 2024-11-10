c6dce196b8e8d2c2d2b0450f3266d7afedcde724
GRACENOBLE <gracenoble72@gmail.com>
2024-11-10 19:51:51 +0300

    feat(mod): added dependencies with go get

--- main.go
+++ main.go
@@ -1,57 +1,7 @@
 package main
 
-// import (
-// 	"bufio"
-// 	"fmt"
-// 	"github.com/GRACENOBLE/expense-tracker/cmd"
-// 	"log"
-// 	"os"
-// 	"strconv"
-// 	"strings"
-// )
-
-import "github.com/spf13/cobra"
+import "github.com/GRACENOBLE/expense-tracker/cmd"
 
 func main() {
-
-	// reader := bufio.NewReader(os.Stdin)
-	// for {
-
-	// 	fmt.Print("expense-tracker:")
-
-	// 	input, _ := reader.ReadString('\n')
-
-	// 	parts := strings.Fields(input)
-
-	// 	switch parts[0] {
-
-	// 	case "add":
-	// 		if len(parts) == 3 {
-
-	// 			ammount, err := strconv.ParseInt(parts[2], 10, 64)
-	// 			if err != nil {
-	// 				log.Fatal(err)
-	// 			}
-	// 			cmd.AddExpense(parts[1], ammount)
-
-	// 			return
-
-	// 		} 
-
-	// 		fmt.Println("Invalid command-line Arguments")
-			
-	// 	case "list":
-
-	// 		cmd.ListExpenses()
-
-	// 	case "summary":
-
-	// 		cmd.SummarizeExpenses()
-
-	// 	case "delete":
-	// 		// cmd.DeleteExpense()
-	// 	default:
-	// 		fmt.Println("Command not recognized, try \"help\"")
-	// 	}
-	// }
+	cmd.Execute()
 }
