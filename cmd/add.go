fcc4dc835998433b72156b2e64a9af60ed1ea6c9
GRACENOBLE <gracenoble72@gmail.com>
2024-11-11 22:26:24 +0300

    feat(add): add boilerplate

---
+++ cmd/add.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(addExpense)
+}
+
+var addExpense = &cobra.Command{
+	Use:   "add",
+	Short: "Add an expense to the JSON",
+	Long:  "This command takes in command line arguments and uses them to create an ",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Adding expense")
+	},
+}
