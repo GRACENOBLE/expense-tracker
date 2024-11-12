bc5e06cba21c86c1188ac345f19c3c27f54aafc6
GRACENOBLE <gracenoble72@gmail.com>
2024-11-12 06:56:32 +0300

    feat(add): can add an expense

--- cmd/add.go
+++ cmd/add.go
@@ -2,6 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"strconv"
+	"github.com/GRACENOBLE/expense-tracker/pkg/helpers"
+	"github.com/GRACENOBLE/expense-tracker/pkg/types"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +18,54 @@ var addExpense = &cobra.Command{
 	Short: "Add an expense to the JSON",
 	Long:  "This command takes in command line arguments and uses them to create an ",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Adding expense")
+		if len(args) != 2 {
+			log.Fatal("Invalid number of arguents")
+		}
+
+		expenseBody := args[0]
+
+		expenseAmount, err := strconv.ParseInt(args[1], 10, 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//Ensure the file exists
+		helpers.CreateFileIfNotExists("output/expenses.json")
+
+		// Check if the file is empty
+		empty, err := helpers.IsFileEmpty("output/expenses.json")
+		if err != nil {
+			log.Fatalf("Failed to check if file is empty: %v", err)
+		}
+
+		// If the file is empty, initialize with the new expense
+		if empty {
+			//create a new expense
+			expense := helpers.CreateNewExpense(1, expenseBody, expenseAmount)
+			if err := helpers.WriteExpensesToFile("output/expenses.json", []types.Expense{expense}); err != nil {
+				log.Fatalf("Failed to write new expense: %v", err)
+			}
+		} else {
+			//count expenses
+			expenses, err := helpers.ReadExpensesFromFile("output/expenses.json")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			expenseCount := len(expenses) + 1
+
+			//create new expeanse
+			expense := helpers.CreateNewExpense(expenseCount, expenseBody, expenseAmount)
+
+			// Append the new expense
+			newData := append(expenses, expense)
+
+			// Write the updated data back to the file
+			if err := helpers.WriteExpensesToFile("output/expenses.json", newData); err != nil {
+				log.Fatalf("Failed to write updated expenses: %v", err)
+			}
+		}
+
+		fmt.Println("Added Successfully.")
 	},
 }
--- pkg/helpers/factories.go
+++ pkg/helpers/factories.go
@@ -8,8 +8,8 @@ import (
 )
 
 // Factory to create a new expense
-func CreateNewExpense(id int, description string, ammount int64) *types.Expense {
-	return &types.Expense{
+func CreateNewExpense(id int, description string, ammount int64) types.Expense {
+	return types.Expense{
 		ID:          int64(id),
 		Date:        time.Now().Format("2006-01-02 15:04:05"),
 		Description: description,
