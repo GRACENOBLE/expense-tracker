0640ae5494968512ec4b492abb25db3a2eeab976
GRACENOBLE <gracenoble72@gmail.com>
2024-11-12 08:27:07 +0300

    feat(list): can list expenses

--- cmd/list.go
+++ cmd/list.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/GRACENOBLE/expense-tracker/pkg/types"
 	"github.com/spf13/cobra"
 )
 
@@ -14,6 +19,21 @@ var listExpenses = &cobra.Command{
 	Short: "Long list all tasks",
 	Long:  "This function lists all tasks in details.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Listing expenses")
+		jsonData, err := os.ReadFile("output/expenses.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		var expenses []types.Expense
+
+		err = json.Unmarshal(jsonData, &expenses)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("ID\tDate\t\t\tDescription\tAmount")
+		for _, expense := range expenses {
+			fmt.Printf("%v\t%v\t%v\t%v\n", expense.ID, expense.Date, expense.Description, expense.Amount)
+		}
+
 	},
 }
